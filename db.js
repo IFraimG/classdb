b9510922bd721606d5b675736f6a7f28ac42fc84
IFramex <kulsasha55@gmail.com>
2021-11-07 10:08:15 +0300

    fix5

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ module.exports = () => {
 
 
   const connection = mysql.createConnection({
-    host: process.env.QOVERY_DATABASE_test2_HOST || null,
-    user: process.env.QOVERY_DATABASE_test2_USERNAME || "root",
+    host: process.env.QOVERY_MYSQL_Z526D0133_HOST || null,
+    user: process.env.QOVERY_MYSQL_Z526D0133_LOGIN || "root",
     database: process.env.QOVERY_APPLICATION_Z4BA1D35E_NAME || "test2",
     password: process.env.QOVERY_MYSQL_Z526D0133_PASSWORD || process.env.DB_PASSWORD,
     uri: process.env.QOVERY_MYSQL_Z526D0133_DATABASE_URL || null,
