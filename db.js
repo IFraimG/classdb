fce7f3dd695f8ca80248044ecbd176323b68f95d
IFramex <kulsasha55@gmail.com>
2021-11-07 09:42:34 +0300

    fix2

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 
 
 const connection = mysql.createConnection({
-  host: process.env.QOVERY_DATABASE_test2_HOST || "localhost",
+  host: process.env.QOVERY_DATABASE_test2_HOST || null,
   user: process.env.QOVERY_DATABASE_test2_USERNAME || "root",
   database: "test2",
-  password: process.env.QOVERY_DATABASE_test2_PASSWORD || process.env.DB_PASSWORD
+  password: process.env.QOVERY_MYSQL_Z526D0133_PASSWORD || process.env.DB_PASSWORD,
+  uri: process.env.QOVERY_MYSQL_Z526D0133_DATABASE_URL || null,
+  port: process.env.QOVERY_MYSQL_Z526D0133_PORT || 3306
 });
 
 connection.connect(err => {
